d83d7945ae201c404dcdb1fbb9e28a576cc99c74
Felix Wegener <Felix.Wegener05@t-online.de>
2023-12-24 14:09:05 +0100

    change log from custom file to logFile

--- Pythonlog.py
+++ Pythonlog.py
@@ -2,6 +2,7 @@ import my_time as my_time
 
 
 def info(message):
-    f = open("/home/pi/project/SmartHomeBackend/log.txt", "a")
-    f.write(my_time.getCurrentTime() + "    " + message + "\n")
-    f.close()
+    print(my_time.getCurrentTime() + message)
+    # f = open("/home/pi/project/SmartHomeBackend/log.txt", "a")
+    # f.write(my_time.getCurrentTime() + "    " + message + "\n")
+    # f.close()
